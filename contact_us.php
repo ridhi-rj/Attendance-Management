10321b99413f5d87c9fceb9c3d3cc10856f38bcc
ridhi-rj <121192490+ridhi-rj@users.noreply.github.com>
2024-05-09 19:03:54 +0530

    Add files via upload

---
+++ contact_us.php
@@ -0,0 +1,34 @@
+<?php
+    include './Database/Controler.php';
+    include 'role.php';
+?>
+
+<h2>Contact Details</h2> 
+<div class="row">
+          <div class="col-md-12">
+          <div class="panel panel-default">
+          <div class="panel-body">
+          <div class="table-responsive">
+          
+         <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                   <td><center><br><img src="C:\xampp\htdocs\project\img\ridhi.jpg" height="20%" width="30%" style="border-radius:50%;"></center><br>
+                        <b><h3><center>RIDHI RAJPUT</center></h3></b>
+                        <h4><center><b>Email:</b> ridhirajput333@gmail.com</center>
+                        <h4><center><b>Contact Number:</b>123456789</center>
+                   </td>
+                   <td> <center><br><img src="" height="20%" width="28%" style="border-radius:50%;"></center>  
+                         <br>
+				<b><h3><center>NAVIN KUMAR YADAV</center></h3></b>
+                                <h4><center><b>Email:</b> ynavinkumar7@gmail.com</center>
+                                <h4><center><b>Contact Number:</b>123456789</center>
+                   </td>
+                   <td> <center><br><img src="img/c.jpg" height="20%" width="28%" style="border-radius:50%;"></center>  
+                         <br>
+				<b><h3><center>PAMMI KUMARI</center></h3></b>
+                                <h4><center><b>Email:</b> soni.chandni.415@gmail.com</center>
+                                <h4><center><b>Contact Number:</b>123456789</center>
+                   </td>
+          </table>
+<?php
+include 'footer.php';
+?>
